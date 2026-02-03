9e1dc353d08339e780b2a5aa59f45992510c0b21
septenmale <zavhorodnii.viktor@icloud.com>
2026-02-03 11:51:47 +0100

    Initial Commit

---
+++ GutenBooks/GutenBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  GutenBooksApp.swift
+//  GutenBooks
+//
+//  Created by Viktor Zavhorodnii on 03/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct GutenBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
